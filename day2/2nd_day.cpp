1081f6c134ff7796904bef02a65123c647e9a32a
Sathi7029 <sathiruidas7029@gmail.com>
2022-01-02 14:22:59 +0530

    2nd and 3rd day
    2nd_day
    3rd_day

---
+++ day2/2nd_day.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+void copy_array(int arr1[],int arr2[],int n)
+{
+    int i;
+    for(i=0;i<n;i++)
+    {
+        cin>>arr1[i];
+    }
+    /*copied*/
+    for(i=0;i<n;i++)
+    {
+        arr2[i]=arr1[i];
+    }
+    /*display the copied array*/
+    for(i=0;i<n;i++)
+    {
+        cout<<arr2[i]<<"  ";
+    }
+}
+int main()
+{
+    int arr1[20],arr2[20],n;
+    cout<<"\nenter the array size:";
+    cin>>n;
+    copy_array(arr1,arr2,n);
+    return 0;
+}
---
+++ day3/3rd_day.cpp
@@ -0,0 +1,23 @@
+/*change upper to lawer case */
+#include<iostream>
+#include<conio.h>
+using namespace std;
+void character(char cha)
+{
+    if(cha>='A' && cha<='Z')
+    {
+        cout<<"\nit is in upprer case,its lawer case is:"<<(cha+32);
+    }
+    else
+    {
+        cout<<"\n it is in lawer case ,upper case is:"<<(cha-32);
+    }
+}
+int main()
+{
+    char ch;
+    cout<<"enter any character:";
+    cin>>ch;
+    character(ch);
+    getch();
+}
