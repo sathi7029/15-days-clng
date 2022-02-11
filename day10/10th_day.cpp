843926cd1f1f2eaa1e259d7f097d4003509dbfbf
Sathi7029 <sathiruidas7029@gmail.com>
2022-02-11 20:51:59 +0530

    Create 10th_day.cpp

---
+++ day10/10th_day.cpp
@@ -0,0 +1,27 @@
+//index of smallest element
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,a[20],min,min_pos;
+    cout<<"\nenter the array size:";
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    min=a[0];
+    min_pos=0;
+    for(i=1;i<n;i++)
+    {
+        if(a[i]<min)
+        {
+            min=a[i];
+            min_pos=i;
+        }
+    }
+    cout<<"\nsmllest element is:"<<min;
+    cout<<"\nsmallest element index:"<<min_pos;
+    return 0;
+    
+}
